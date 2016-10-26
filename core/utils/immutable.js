8ad53f70d3b515c93e067ca790e9fadb1591ae2c
ente <ducksource@duckpond.ch>
2016-10-26 18:25:57 +0200

    Immutable uses better fromJS()
    * fromJS was not converting ES6 objects properly

--- core/utils/immutable.js
+++ core/utils/immutable.js
@@ -3,5 +3,28 @@
  */
 
 const immutableJS = require('immutable');
-module.exports.immutable = function immutable (v) { return immutableJS.fromJS(v); };
+
+/**
+ * from: https://github.com/facebook/immutable-js/wiki/Converting-from-JS-objects
+ */
+function fromJSGreedy (js) {
+  return typeof js !== 'object' || js === null
+    ? js : Array.isArray(js)
+      ? immutableJS.Seq(js).map(fromJSGreedy).toList()
+      : immutableJS.Seq(js).map(fromJSGreedy).toMap();
+}
+
+/**
+ * from: https://github.com/facebook/immutable-js/wiki/Converting-from-JS-objects
+ */
+/* eslint-disable no-unused-vars */
+function fromJSOrdered (js) {
+  return typeof js !== 'object' || js === null
+    ? js : Array.isArray(js)
+      ? immutableJS.Seq(js).map(fromJSOrdered).toList()
+      : immutableJS.Seq(js).map(fromJSOrdered).toOrderedMap();
+}
+/* eslint-enable no-unused-vars */
+
+module.exports.immutable = function immutable (v) { return fromJSGreedy(v); };
 module.exports.mutable = function mutable (v) { return (v.toJS) ? v.toJS() : v; };
