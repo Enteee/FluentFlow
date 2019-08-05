d4b1d8523965734fcbecdd5544c3097051c7b4f7
Enteee <ducksource@duckpond.ch>
2019-08-05 21:18:20 +0200

    Add default.nix (#7)

---
+++ default.nix
@@ -0,0 +1,24 @@
+with import <nixpkgs> {};
+
+let
+
+in stdenv.mkDerivation {
+  name = "fluentflow";
+
+  src = null;
+
+  buildInputs = [
+    nodejs
+  ];
+
+  shellHook = ''
+    export NPM_CONFIG_PREFIX="$(${mktemp.outPath}/bin/mktemp -d -t npm_XXXXXXX)"
+    export PATH="''${PATH}:''${NPM_CONFIG_PREFIX}/bin"
+    npm set prefix "''${NPM_CONFIG_PREFIX}/.npm-global"
+  '';
+
+  exitHook = ''
+    rm -rf ''${NPM_CONFIG_PREFIX}
+  '';
+
+}
